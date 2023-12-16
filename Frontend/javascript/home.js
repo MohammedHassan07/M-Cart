7a95da99613555d1c2df6b257c4776ea88242000
MohammedHassan07 <mhassancs260@gmail.com>
2023-12-16 12:01:12 -0800

    feat: Login form is implementd

--- Frontend/javascript/home.js
+++ Frontend/javascript/home.js
@@ -1,1 +1,9 @@
 console.log('Home.js')
+
+const backToTop = document.getElementById('back-to-top')
+backToTop.addEventListener('click', (event) => {
+
+    event.preventDefault()
+
+    console.log('back to Top')
+})
